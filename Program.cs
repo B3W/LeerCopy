65e2d29dbaebd7af1e27ae6681fb60b59d0050d4
Weston Berg <westieberg@gmail.com>
2017-12-20 21:41:34 -0600

    Update Program.cs
    Added copyright and license notices

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿// Copyright 2017 Weston Berg (westieberg@gmail.com)
+//
+// This file is part of Leer Copy.
+//
+// Leer Copy is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Leer Copy is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Leer Copy.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
 using System.Threading;
 using System.IO;
 using System.Windows.Forms;
@@ -8,7 +25,6 @@ namespace Leer_Copy
     static class Program
     {
         /// <summary>
-        /// Author: Weston Berg (weberg@iastate.edu)
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
